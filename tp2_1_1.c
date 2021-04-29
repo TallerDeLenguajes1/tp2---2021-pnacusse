49c0f2c91bf53faab165f993cff97481f76999e7
Pablo Nacusse <grandnacusse@gmail.com>
2021-04-29 09:55:32 -0300

    primer commit

---
+++ tp2_1_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define N 4
+#define M 5
+int f,c;
+double mt[N][M];
+
+int main()
+{
+    int f,c;
+    double mt[N][M];
+
+    for(f = 0; f<N; f++)
+    {
+        for(f = 0; f<N; f++)
+        {
+            printf("%lf\t" , mt[f][c]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
